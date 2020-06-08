7794a1c92cd96240797a7e7cf2858fcee4915bd7
zwarshavsky <zwarshavsky@gmail.com>
2020-06-07 20:18:42 -0400

    training sample model

--- kaggle-explore/modules/data_helper.py
+++ kaggle-explore/modules/data_helper.py
@@ -348,7 +348,7 @@ def get_jpeg_data_files_paths():
         The input file paths as list [train_jpeg_dir, test_jpeg_dir, test_jpeg_additional, train_csv_file]
     """
 
-    data_root_folder = os.path.abspath("../input/")
+    data_root_folder = os.path.abspath("../kaggle-explore/input/")
     train_jpeg_dir = os.path.join(data_root_folder, 'train-jpg')
     test_jpeg_dir = os.path.join(data_root_folder, 'test-jpg')
     test_jpeg_additional = os.path.join(data_root_folder, 'test-jpg-additional')
