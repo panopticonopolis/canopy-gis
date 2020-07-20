fe60ffee1f50d725eb20143fb9869da90a1fbc10
zwarshavsky <zwarshavsky@gmail.com>
2020-07-20 17:46:19 -0400

    sentinel download

--- data_collection/env_vars.py
+++ data_collection/env_vars.py
@@ -1,2 +1,6 @@
-username = "zwarshavsky"
-password = "QGA4sH_kWjUcrsf"
+landsat_username = "zwarshavsky"
+landsat_password = "QGA4sH_kWjUcrsf"
+
+sentinel_username = "zwarshavsky"
+sentinel_password = "laughter83"
+
